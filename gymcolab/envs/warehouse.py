fcbfe1d412aa1326e5a7263b1358447e9b8d2270
TolgaOk <tolgaokk@gmail.com>
2019-08-22 23:49:35 +0300

    Warehouse expects worldmap

--- gymcolab/envs/warehouse.py
+++ gymcolab/envs/warehouse.py
@@ -8,6 +8,7 @@ import random
 import pycolab
 import time
 from collections import defaultdict
+from itertools import chain
 from pycolab.prefab_parts import sprites
 from pycolab.ascii_art import Partial
 from gymcolab.colab_env import ColabEnv
@@ -58,21 +59,9 @@ class PlayerSprite(sprites.MazeWalker):
 class WallDrape(pycolab.things.Drape):
     def __init__(self, curtain, character):
         super().__init__(curtain, character)
-        self.env_length = ENV_LENGTH
 
     def update(self, actions, board, layers, backdrop, things, the_plot):
-        del actions, board, layers, backdrop
-        # --------- Termination -------------
-        inventory_size = sum(things["P"].inventory.values())
-        n_balls = sum(thing.curtain.any() for thing in things.values()
-                      if isinstance(thing, BallDrape))
-        if inventory_size == 0 and n_balls == 0:
-            the_plot.terminate_episode()
-        
-        self.env_length -= 1
-        if self.env_length <= 0:
-            the_plot.terminate_episode()
-        # -----------------------------------
+        del self, actions, board, layers, backdrop, things, the_plot
 
 
 class BallDrape(pycolab.things.Drape):
@@ -99,6 +88,9 @@ class BucketDrape(pycolab.things.Drape):
         assert self.character.isupper(), ("Character for a Bucket must be"
                                           "uppercase!")
         self.pairing = pairing
+        self.env_length = ENV_LENGTH
+        self.ball_of_interest = list(
+            chain(*self.pairing.values()))
 
     def update(self, actions, board, layers, backdrop, things, the_plot):
 
@@ -110,6 +102,20 @@ class BucketDrape(pycolab.things.Drape):
                 n_balls += things["P"].inventory[ball_char]
                 things["P"].inventory[ball_char] = 0
             the_plot.add_reward(n_balls * REWARD)
+        # -----------------------------------
+        # --------- Termination -------------
+        inventory_size = sum(value for ball, value in
+                             things["P"].inventory.items()
+                             if ball in self.ball_of_interest)
+        n_balls = sum(thing.curtain.any() for thing in things.values()
+                      if isinstance(thing, BallDrape) and
+                      thing.character in self.ball_of_interest)
+        if inventory_size == 0 and n_balls == 0:
+            the_plot.terminate_episode()
+
+        self.env_length -= 1
+        if self.env_length <= 0:
+            the_plot.terminate_episode()
         # -----------------------------------
 
 
@@ -127,36 +133,67 @@ class Warehouse(ColabEnv):
     COLORS = {
         "P": "#00B8FA",
         " ": "#DADADA",
-        "b": "#DADA22",
-        "c": "#DA22DA",
+        "b": "#11C9C9",
+        "c": "#00B8B8",
+        "d": "#22DADA",
         "B": "#DAA822",
         "#": "#989898"
     }
 
-    def __init__(self, pairing=None, worldmap=None, cell_size=40, colors=None,
-                 render_croppers=None):
+    def __init__(self, balls, bucket, pairing=None, worldmap=None,
+                 cell_size=40, colors=None, render_croppers=None):
         self.world_map = worldmap or WORLDMAP
         self.pairing = pairing or PAIRING
+        self.balls = balls
+        self.bucket = bucket
+        assert isinstance(bucket, str), "Bucket must be a character"
+        assert isinstance(balls, str), "Balls must be a string of characters"
         super().__init__(cell_size=cell_size,
                          colors=colors or self.COLORS,
                          render_croppers=render_croppers)
 
     def _init_game(self):
+        drapes = {}
+        for ball in self.balls:
+            drapes[ball] = Partial(BallDrape)
+        drapes["#"] = Partial(WallDrape)
+        drapes[self.bucket] = Partial(BucketDrape, self.pairing)
+
+        u_schedule = ([["P"], ["#"], [self.bucket]] +
+                      [[char] for char in self.balls])
+
+        z_order = "P" + self.balls + self.bucket + "#"
+
         game = pycolab.ascii_art.ascii_art_to_game(
             art=self.world_map,
             what_lies_beneath=" ",
             sprites={"P": Partial(PlayerSprite)},
-            drapes={"#": Partial(WallDrape),
-                    "b": Partial(BallDrape),
-                    "B": Partial(BucketDrape, self.pairing)},
-            update_schedule=[["P"], ["b"], ["B"], ["#"]],
-            z_order="PbB#"
+            drapes=drapes,
+            update_schedule=u_schedule,
+            z_order=z_order
         )
         return game
 
 
 if __name__ == "__main__":
-    env = Warehouse(pairing=PAIRING, worldmap=WORLDMAP)
+
+    worldmap = ["##########",
+                "#     B  #",
+                "#   c    #",
+                "#        #",
+                "#    P   #",
+                "#  b     #",
+                "#      c #",
+                "#  d     #",
+                "#    d   #",
+                "##########"]
+
+    pairing = {
+        "B": ["c"],
+    }
+
+    env = Warehouse(balls="cdb", bucket="B",
+                    pairing=pairing, worldmap=worldmap)
     for i in range(800):
         done = False
         state = env.reset()
@@ -165,6 +202,7 @@ if __name__ == "__main__":
             state, reward, done, _ = env.step(action)
             env.render()
             print(reward, done)
+            print(state.shape)
             time.sleep(0.1)
             if done:
                 print("Done")
